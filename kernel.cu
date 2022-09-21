5de316551e29dcd6387e7de93e44584fee1fccf9
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-09-21 14:21:06 +0300

    hh

--- kernel.cu
+++ kernel.cu
@@ -10,7 +10,7 @@ __global__ void mm_tiled_kernel(float* A, float* B, float* C, unsigned int M, un
     unsigned int row = blockIdx.y*blockDim.y + threadIdx.y;
     unsigned int col = blockIdx.x*blockDim.x + threadIdx.x;
     float sum = 0.0f;
-    for(unsigned int tile = 0; tile < (K + TILE_DIM -1 ) / TILE_DIM; ++tile) {
+    for(unsigned int tile = 0; tile < (N + TILE_DIM -1 ) / TILE_DIM; ++tile) {
 
         if( row < M && tile*TILE_DIM+threadIdx.x < K){
         A_s[threadIdx.y][threadIdx.x] = A[row*K + tile*TILE_DIM + threadIdx.x];
@@ -18,7 +18,7 @@ __global__ void mm_tiled_kernel(float* A, float* B, float* C, unsigned int M, un
         else{
         A_s[threadIdx.y][threadIdx.x] = 0;
         }
-        if( TILE_DIM*tile+threadIdx.y < N && col < K){
+        if( TILE_DIM*tile+threadIdx.y < K && col < N){
         
         B_s[threadIdx.y][threadIdx.x] = B[(tile*TILE_DIM + threadIdx.y)*K + col];
         }

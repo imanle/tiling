0bc906e6dd1fe74875e7fc4785efd62c71120f16
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-09-21 13:41:48 +0300

    heh

--- kernel.cu
+++ kernel.cu
@@ -15,9 +15,15 @@ __global__ void mm_tiled_kernel(float* A, float* B, float* C, unsigned int M, un
         if( row < M && tile*TILE_DIM+threadIdx.x < K){
         A_s[threadIdx.y][threadIdx.x] = A[row*K + tile*TILE_DIM + threadIdx.x];
         }
+        else{
+        A_s[threadIdx.y][threadIdx.x] = 0;
+        }
         if( TILE_DIM*tile+threadIdx.y < N && col < N){
         
         B_s[threadIdx.y][threadIdx.x] = B[(tile*TILE_DIM + threadIdx.y)*N + col];
+        }
+        else{
+        B_s[threadIdx.y][threadIdx.x] = 0;
         }
         
 __syncthreads();
